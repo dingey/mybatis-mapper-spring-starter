b33ee4cfd04d8c0e99a21d5fd446ede851b84cf2
d <d>
2019-05-20 11:11:22 +0800

    fix bug

--- src/main/java/com/github/dingey/mybatis/mapper/MybatisMapperConfiguration.java
+++ src/main/java/com/github/dingey/mybatis/mapper/MybatisMapperConfiguration.java
@@ -1,24 +1,31 @@
 package com.github.dingey.mybatis.mapper;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author d
  */
 @Configuration
-@EnableConfigurationProperties(MybatisMapperProperties.class)
+@ConditionalOnBean({SqlSessionFactory.class})
+@EnableConfigurationProperties({MybatisMapperProperties.class})
 public class MybatisMapperConfiguration {
-    @Resource
-    private MybatisMapperProperties properties;
+	@Resource
+	private MybatisMapperProperties properties;
+	@Resource
+	private List<SqlSessionFactory> sqlSessionFactoryList;
 
-    @Bean
-    @ConditionalOnMissingBean(InsertInterceptor.class)
-    public InsertInterceptor insertInterceptor() {
-        return new InsertInterceptor(properties);
-    }
+	@PostConstruct
+	public void addPageInterceptor() {
+		InsertInterceptor insertInterceptor = new InsertInterceptor(properties);
+		for (SqlSessionFactory sqlSessionFactory : this.sqlSessionFactoryList) {
+			sqlSessionFactory.getConfiguration().addInterceptor(insertInterceptor);
+		}
+	}
 }
